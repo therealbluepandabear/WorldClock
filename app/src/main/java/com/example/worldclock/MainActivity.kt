27de0ac9947b42bad803c418f99202cde9c8081f
xTheCoderx <justaguyoney@gmail.com>
2021-10-07 20:45:04 +1300

    Added setBindings() function.

--- app/src/main/java/com/example/worldclock/MainActivity.kt
+++ app/src/main/java/com/example/worldclock/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.worldclock
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.worldclock.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setBindings()
+    }
+
+    private fun setBindings() {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
